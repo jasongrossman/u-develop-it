599dae764de20427dca7083b700f241d4ad74908
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-09-16 13:47:53 -0400

    added db query

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
+//requirements and port connection
+const mysql = require('mysql2');
 const express = require('express');
+const { createConnection } = require('mysql2/promise');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//express.js middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//mysql database connection
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'frogulon',
+        database: 'election'
+    },
+    console.log('Connected to the election database.'),
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
+//catchall route
 app.use((req, res) => {
     res.status(404).end();
   });
