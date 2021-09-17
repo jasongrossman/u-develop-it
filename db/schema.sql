c9859bba454901d1b462090093dee177cc5e3fbc
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-09-17 16:30:11 -0400

    added voters table and seed data

--- db/schema.sql
+++ db/schema.sql
@@ -12,3 +12,11 @@ CREATE TABLE parties (
     name VARCHAR(50) NOT NULL,
     description TEXT
 );
+
+CREATE TABLE voters (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
